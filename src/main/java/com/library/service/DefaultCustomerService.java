df72bd910ab373d904717c5a4edbb540e3f71905
Bartłomiej Jonda <bartlomiej.jonda@gmail.com>
2019-01-26 12:43:53 +0100

    Almost configured spring

--- src/main/java/com/library/service/DefaultCustomerService.java
+++ src/main/java/com/library/service/DefaultCustomerService.java
@@ -1,16 +1,26 @@
 package com.library.service;
 
 import com.library.model.Customer;
+import com.library.repository.interfaces.CustomerFinder;
 import com.library.repository.interfaces.CustomerRepository;
 import com.library.service.interfaces.CustomerService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DefaultCustomerService implements CustomerService {
     private final CustomerRepository customerRepository;
+    private final CustomerFinder customerFinder;
 
-    public DefaultCustomerService(CustomerRepository customerRepository) {
+    public DefaultCustomerService(CustomerRepository customerRepository, CustomerFinder customerFinder) {
         this.customerRepository = customerRepository;
+        this.customerFinder = customerFinder;
+    }
+
+    @Override
+    public List<Customer> showCustomers() {
+        return customerFinder.getAll();
     }
 
     @Override
